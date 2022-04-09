815599bef4ef09365d62d2cd63431a2308891e21
Maisie James <maisienjames@gmail.com>
2022-04-09 11:56:45 +0100

    expanded growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -8,16 +8,26 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.name).toEqual('Fido');
     });
-    it('has a initial age of 0', () => {
+    it('has initial age of 0', () => {
     const pet = new Pet('Fido');
     expect(pet.age).toEqual(0);
     });
+    it('has initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+    });
+    it('has initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+    });
     });
   describe('growUp', () => {
-    it('increments the age by 1', () => {
+    it('increments the age by 1, increments hunger by 5, decreases fitness by 3', () => {
       const pet = new Pet('Fido');
       pet.growUp();
       expect(pet.age).toEqual(1);
+      expect(pet.fitness).toEqual(7);
+      expect(pet.hunger).toEqual(5);
     });
   });
 
